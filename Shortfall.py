27601b89a1e48da1bfcd4136a6eb321a4ac3cc8e
Tiriosh1007 <49543074+Tiriosh1007@users.noreply.github.com>
2024-06-17 14:55:20 +0800

    bonesetter hotfix

--- Shortfall.py
+++ Shortfall.py
@@ -110,7 +110,7 @@ class Shortfall():
 
     bone_pos_list = []
     for n00 in t_df.loc[t_df['benefit'].str.contains('Herbalist', case=False)].index:
-      if 'Bonsetter' in t_df['benefit'].iloc[n00+1]:
+      if 'Bonsetter' in t_df['benefit'].iloc[n00+1] and n00 < len(t_df.benefit):
         t_df['no_of_claims'].iloc[n00] = t_df['no_of_claims'].iloc[n00] + t_df['no_of_claims'].iloc[n00 + 1]
         t_df['no_of_claimants'].iloc[n00] = t_df['no_of_claimants'].iloc[n00] + t_df['no_of_claimants'].iloc[n00 + 1]
         t_df['incurred_amount'].iloc[n00] = t_df['incurred_amount'].iloc[n00] + t_df['incurred_amount'].iloc[n00 + 1]
