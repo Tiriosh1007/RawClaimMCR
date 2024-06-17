bf05f2a30fd2f04f5d309aa7fed5cdca7aaea110
Tiriosh1007 <49543074+Tiriosh1007@users.noreply.github.com>
2024-06-17 16:14:26 +0800

    suboffice test 1

--- RawClaimData.py
+++ RawClaimData.py
@@ -840,7 +840,7 @@ class RawClaimData():
 
     return None
 
-  def mcr_p20_policy(self, by=None):
+  def mcr_p20_policy(self, by='suboffice'):
     if by == None:
       __p20_policy_df_col = ['policy_number', 'year', 'incurred_amount', 'paid_amount']
       __p20_policy_group_col = ['policy_number', 'year']
