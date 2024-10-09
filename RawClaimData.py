adaac2c4b6c683003c861fa360d6861db26f3dca
Tiriosh1007 <49543074+Tiriosh1007@users.noreply.github.com>
2024-10-09 09:47:37 +0800

    try try

--- RawClaimData.py
+++ RawClaimData.py
@@ -1821,7 +1821,7 @@ class RawClaimData():
     __freq_df['Physio + Chiro'] = __freq_df[['Chiro (CT)', 'Physio (PT)']].sum(axis=1)
 
     __freq_df = __freq_df.reset_index()
-    __freq_sum_benefit_df = self.df[['claimant', 'benefit', 'paid_amount']].loc[self.df.benefit.isin(total_visit_col)].groupby(['claimant', 'benefit']).mean().rename(columns={'paid_amount': 'total_paid'}).unstack().set_index('claimant')
+    __freq_sum_benefit_df = self.df[['claimant', 'benefit', 'paid_amount']].loc[self.df.benefit.isin(total_visit_col)].groupby(['claimant', 'benefit']).mean().rename(columns={'paid_amount': 'total_paid'}).unstack().reset_index()
     __freq_sum_benefit_df.columns = [f'{b} paid per claim' for b in total_visit_col]
     __freq_sum_df = self.df[['claimant', 'paid_amount']].loc[self.df.benefit.isin(total_visit_col)].groupby(['claimant']).sum().rename(columns={'paid_amount': 'total_paid'})
     print(__freq_sum_benefit_df.columns)
