23aec1055263e092c973a7fc7350e6b86564bab4
Tiriosh1007 <49543074+Tiriosh1007@users.noreply.github.com>
2024-10-22 10:58:15 +0800

    try

--- RawClaimData.py
+++ RawClaimData.py
@@ -1854,8 +1854,7 @@ class RawClaimData():
     self.ip_usage = self.df[['policy_number', 'year', 'suboffice', 'claimant', 'class', 'dep_type', 'age', 'benefit', 'diagnosis', 'paid_amount']] \
     .loc[self.df.benefit_type.str.contains('hosp', case=False)] \
     .groupby(by=['policy_number', 'year', 'suboffice', 'claimant', 'class', 'dep_type', 'age', 'diagnosis', 'benefit']).sum().unstack()
-    print(self.ip_usage.columns.tolist())
-    cols = [f.remove('paid_amount.') for f in self.ip_usage.columns.tolist()]
+    cols = [f.remove('paid_amount.') for f in self.ip_usage.columns]
     self.ip_usage.columns = cols
 
    
