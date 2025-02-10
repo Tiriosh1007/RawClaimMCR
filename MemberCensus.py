e8a153ea73003b1eeb33168e95081aa3fa4b54b3
Tiriosh1007 <49543074+Tiriosh1007@users.noreply.github.com>
2025-02-10 16:41:00 +0800

    tr

--- MemberCensus.py
+++ MemberCensus.py
@@ -153,8 +153,8 @@ class MemberCensus():
                              bins=self.age_range, 
                              right=False, 
                              )
-                temp_df = pd.DataFrame(dis.value_counts().sort_index(), columns=[f"{gender}_{dep}"])
-                self.gender_dis_df = pd.concat([self.gender_dis_df, temp_df], axis=1, ignore_index=False)
+                self.gender_dis_df = pd.DataFrame(dis.value_counts().sort_index(), columns=[f"{gender}_{dep}"])
+                # self.gender_dis_df = pd.concat([self.gender_dis_df, temp_df], axis=1, ignore_index=False)
                 print(self.gender_dis_df)
         return       
 
