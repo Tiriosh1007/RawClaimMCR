06bf02d5caff21038e38878388a72e1c3130f568
Tiriosh1007 <49543074+Tiriosh1007@users.noreply.github.com>
2025-02-10 16:05:37 +0800

    fix

--- MemberCensus.py
+++ MemberCensus.py
@@ -146,7 +146,7 @@ class MemberCensus():
         self.gender_dis_df = pd.DataFrame()
         for gender in self.gender:
             for dep in self.dep_type:
-                dis = pd.cut(self.member_df.loc[(self.member_df['gender'] == gender) & (self.member_df['dep_type'] == dep)], 
+                dis = pd.cut(self.member_df['age'].loc[(self.member_df['gender'] == gender) & (self.member_df['dep_type'] == dep)], 
                              bins=self.age_range, 
                              right=False, 
                              labels=self.age_lbs)
