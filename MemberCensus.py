931d4c01b69ab5d8b4cae772b74adb8effa1001a
Tiriosh1007 <49543074+Tiriosh1007@users.noreply.github.com>
2025-02-10 16:36:43 +0800

    tryh

--- MemberCensus.py
+++ MemberCensus.py
@@ -161,8 +161,8 @@ class MemberCensus():
         temp_df = self.gender_dis_df.copy(deep=True)
         temp_df[['M_EE', 'M_SP', 'M_CH']] = -temp_df[['M_EE', 'M_SP', 'M_CH']]
 
-        # y = list(range(0, 100, 10))
-        y = self.age_lbs
+        y = list(range(0, 100, 10))
+        # y = self.age_lbs
         tmax = xmax - xstep
         layout = go.Layout(yaxis=go.layout.YAxis(title='Age'),
                             xaxis=go.layout.XAxis(
