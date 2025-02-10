06e94d6f28b9210f9320e046f298eae4e656c55c
Tiriosh1007 <49543074+Tiriosh1007@users.noreply.github.com>
2025-02-10 16:06:28 +0800

    try

--- MemberCensus.py
+++ MemberCensus.py
@@ -30,7 +30,7 @@ class MemberCensus():
 
         self.gender = ['M', 'F']
         self.age_range = np.arange(0, 100, 10)
-        self.age_lbs = ['{} - <{}'.format(i, i+10) for i in self.age_range]
+        self.age_lbs = ['{} - {}'.format(i, i+10) for i in self.age_range]
         self.dep_type = ['EE','SP', 'CH']
         
     
