ffaae95c3f7463c9485e8df8efd55cbf1a44fe07
Tiriosh1007 <49543074+Tiriosh1007@users.noreply.github.com>
2025-05-26 17:10:15 +0800

    try

--- front_end.py
+++ front_end.py
@@ -808,10 +808,18 @@ if st.session_state.ocr == True:
                 ]
               }
             ]
+            plugins = [
+                {
+                    "id": "file-parser",
+                    "pdf": {
+                        "engine": "pdf-text"  # defaults to "mistral-ocr". See Pricing below
+                    }
+                }
+            ]
             payload = {
               "model": "openai/gpt-4.1-nano",
               "messages": messages,
-              # "plugins": plugins
+              "plugins": plugins
             }
             response = requests.post(url, headers=headers, json=payload)
             if response.status_code != 200:
@@ -819,7 +827,7 @@ if st.session_state.ocr == True:
 
             # Parse the JSON response
             try:
-                result = json.loads(response)
+                result = response.json()
                 st.session_state['ocr_result'] = result
                 st.success("File processed successfully!")
             except ValueError:
