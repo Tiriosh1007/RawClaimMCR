0b8f3cd9575dde6a1ad1633f02594518abb2694a
Tiriosh1007 <49543074+Tiriosh1007@users.noreply.github.com>
2025-05-26 14:00:31 +0800

    try

--- front_end.py
+++ front_end.py
@@ -783,7 +783,7 @@ if st.session_state.ocr == True:
       import os
       temp_dir = tempfile.mkdtemp()
       pdf_path_full = os.path.join(temp_dir, pdf_path)
-      with open(path, "wb") as f:
+      with open(pdf_path_full, "wb") as f:
         f.write(uploaded_file.getvalue())
       base64_pdf = encode_pdf_to_base64(f)
       data_url = f"data:application/pdf;base64,{base64_pdf}"
