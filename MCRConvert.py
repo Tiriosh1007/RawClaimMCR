976b0632257715196a453fbba3556034f86044d4
Tiriosh1007 <49543074+Tiriosh1007@users.noreply.github.com>
2025-05-28 15:57:53 +0800

    try

--- MCRConvert.py
+++ MCRConvert.py
@@ -55,8 +55,8 @@ class MCRConvert():
         self.previous_policy_num, self.current_policy_num = previous_policy_num, current_policy_num
         self.plan_info = self.mcr_p21_class.copy(deep=True)
         self.plan_info.dropna(inplace=True)
-        self.plan_info = self.plan_info["class"].loc[(self.plan_info["policy_number"] == self.current_policy_num) & 
-                                            (self.plan_info["year"] == self.current_year)].tolist()
+        self.plan_info = self.plan_info.loc[(self.plan_info["policy_number"] == self.current_policy_num) & 
+                                            (self.plan_info["year"] == self.current_year), "class"].tolist()
         
     
     def claim_info(self):
