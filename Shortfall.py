8e6d875d282e11a69014880d68aea1f3dc563ca1
Tiriosh1007 <49543074+Tiriosh1007@users.noreply.github.com>
2025-06-25 14:37:58 +0800

    try

--- Shortfall.py
+++ Shortfall.py
@@ -40,20 +40,22 @@ class Shortfall():
 
   def __bupa_shortfall(self, shortfall_fp):
 
-    if ".csv" not in shortfall_fp:
-      t_df = pd.read_excel(shortfall_fp, sheet_name='Report')
-      client_name_ = t_df.iloc[:, 0].loc[t_df.iloc[:, 0].str.contains('Customer', case=False) == True].values[0].split(': ')[1].split('     ')[-1]
-      policy_no_ = t_df.iloc[:, 0].loc[t_df.iloc[:, 0].str.contains('Contract', case=False) == True].values[0].split(': ')[1].split('     ')[-1]
-      start_d_ = pd.to_datetime(t_df.iloc[:, 0].loc[t_df.iloc[:, 0].str.contains('Period', case=False) == True].values[0].split(': ')[1].split(' ')[-5], format='%Y-%m-%d')
-      end_d_ = pd.to_datetime(t_df.iloc[:, 0].loc[t_df.iloc[:, 0].str.contains('Period', case=False) == True].values[0].split(': ')[1].split(' ')[-1], format='%Y-%m-%d')
-      duration_ = (end_d_ - start_d_).days + 1
-      policy_id_ = f'{policy_no_}_{start_d_:%Y%m}'
-    else:
+    if ".csv" in shortfall_fp:
       t_df = pd.read_csv(shortfall_fp, sep=',', encoding='utf-8')
       client_name_ = t_df.iloc[:, 1].loc[t_df.iloc[:, 0].str.contains('Customer', case=False) == True].values[0]
       policy_no_ = t_df.iloc[:, 1].loc[t_df.iloc[:, 0].str.contains('Contract', case=False) == True].values[0]
       start_d_ = pd.to_datetime(t_df.iloc[:, 1].loc[t_df.iloc[:, 0].str.contains('Period', case=False) == True].values[0], format='%Y-%m-%d')
       end_d_ = pd.to_datetime(t_df.iloc[:, 2].loc[t_df.iloc[:, 0].str.contains('Period', case=False) == True].values[0], format='%Y-%m-%d')
+      duration_ = (end_d_ - start_d_).days + 1
+      policy_id_ = f'{policy_no_}_{start_d_:%Y%m}'
+    
+      
+    else:
+      t_df = pd.read_excel(shortfall_fp, sheet_name='Report')
+      client_name_ = t_df.iloc[:, 0].loc[t_df.iloc[:, 0].str.contains('Customer', case=False) == True].values[0].split(': ')[1].split('     ')[-1]
+      policy_no_ = t_df.iloc[:, 0].loc[t_df.iloc[:, 0].str.contains('Contract', case=False) == True].values[0].split(': ')[1].split('     ')[-1]
+      start_d_ = pd.to_datetime(t_df.iloc[:, 0].loc[t_df.iloc[:, 0].str.contains('Period', case=False) == True].values[0].split(': ')[1].split(' ')[-5], format='%Y-%m-%d')
+      end_d_ = pd.to_datetime(t_df.iloc[:, 0].loc[t_df.iloc[:, 0].str.contains('Period', case=False) == True].values[0].split(': ')[1].split(' ')[-1], format='%Y-%m-%d')
       duration_ = (end_d_ - start_d_).days + 1
       policy_id_ = f'{policy_no_}_{start_d_:%Y%m}'
 
