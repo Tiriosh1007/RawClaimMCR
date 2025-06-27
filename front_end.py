bf7b7d75750465f573f54885f5ce23fec46075b9
Tiriosh1007 <49543074+Tiriosh1007@users.noreply.github.com>
2025-06-27 13:09:00 +0800

    try new prompt

--- front_end.py
+++ front_end.py
@@ -1011,14 +1011,14 @@ if st.session_state.ocr == True:
                         file_name=file_name_to_csv,
                         mime="application/vnd.ms-excel")
     
-    if "BlueCross Usage" in prompt_data_options[data_selection]:
-      from BlueCrossUsageReportConvert import *
-      BlueCross = BlueCrossUsageReportConvert(csv_data)
-      BlueCross.convert_to_final_df()
-      st.download_button('BlueCross Usage Excel', 
-                        data=BlueCross.final_df.to_csv(index=False).encode('utf-8'),
-                        file_name="BlueCross_Usage_Report.csv",
-                        mime="application/vnd.ms-excel")
+    # if "BlueCross Usage" in prompt_data_options[data_selection]:
+    #   from BlueCrossUsageReportConvert import *
+    #   BlueCross = BlueCrossUsageReportConvert(csv_data)
+    #   BlueCross.convert_to_final_df()
+    #   st.download_button('BlueCross Usage Excel', 
+    #                     data=BlueCross.final_df.to_csv(index=False).encode('utf-8'),
+    #                     file_name="BlueCross_Usage_Report.csv",
+    #                     mime="application/vnd.ms-excel")
 #     BlueCross.csv_convert()
 
 
