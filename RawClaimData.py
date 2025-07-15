1c05f8744c97bc9488df7ae2c60d42b677c9b49d
Tiriosh1007 <49543074+Tiriosh1007@users.noreply.github.com>
2025-07-15 10:25:55 +0800

    try

--- RawClaimData.py
+++ RawClaimData.py
@@ -2129,6 +2129,20 @@ class RawClaimData():
     self.p29_df_grp_procedure_org = p29_df_grp_procedure_org
     return p29_df_grp_procedure_org
 
+  def mcr_p29b_grp_procedure_network(self, by=None):
+
+    __p29_df_grp_procedure_network_col = by + ['minor_surgeries_grp', 'panel', 'incurred_amount', 'claim_id', 'paid_amount', 'claimant']
+    __p29_group_col = by + ['minor_surgeries_grp', 'panel']
+    __p29_sort_order = len(by) * [True] + [True, True]
+
+    p29_df_grp_procedure_network = self.mcr_df[__p29_df_grp_procedure_network_col].copy(deep=True)
+    p29_df_grp_procedure_network.dropna(subset=['minor_surgeries_grp'], inplace=True)
+
+    p29_df_grp_procedure_network = p29_df_grp_procedure_network.groupby(by=__p29_group_col).agg({'incurred_amount': 'sum', 'paid_amount': 'sum', 'claim_id': 'count', 'claimant': 'nunique'}).rename(columns={'claim_id': 'no_of_claim_id', 'claimant': 'no_of_claimants'})
+    p29_df_grp_procedure_network.sort_index(ascending=__p29_sort_order, inplace=True)
+    self.p29_df_grp_procedure_network = p29_df_grp_procedure_network
+    return p29_df_grp_procedure_network
+
   def mcr_pages(self, by=None, export=False, benefit_type_order=['Hospital', 'Clinic', 'Dental', 'Optical', 'Maternity', 'Total']):
     
     if type(by) is not list and by != None: by = [by]
