4b6f045f48411ed559f0c74b6819828145e523d8
Tiriosh1007 <49543074+Tiriosh1007@users.noreply.github.com>
2025-11-03 11:08:31 +0800

    try

--- RawClaimData.py
+++ RawClaimData.py
@@ -828,9 +828,9 @@ class RawClaimData():
     t_df['benefit_type'].loc[t_df['benefit_type'].str.contains('outpatient', case=False)] = 'Clinic'
 
     t_df.diagnosis.fillna('No diagnosis provided', inplace=True)
-    if len(t_df.diagnosis.unique()) > 2:
-      t_df['benefit_type'].loc[(t_df['diagnosis'].str.contains('dental', case=False)) & (t_df['benefit'].str.contains('clinical', case=False) == False)] = 'Dental'
-      t_df['benefit'].loc[t_df['diagnosis'].str.contains('dental', case=False) & (t_df['benefit'].str.contains('clinical', case=False) == False)] = 'Dental'
+    # if len(t_df.diagnosis.unique()) > 2:
+    #   t_df['benefit_type'].loc[(t_df['diagnosis'].str.contains('dental', case=False)) & (t_df['benefit'].str.contains('clinical', case=False) == False)] = 'Dental'
+    #   t_df['benefit'].loc[t_df['diagnosis'].str.contains('dental', case=False) & (t_df['benefit'].str.contains('clinical', case=False) == False)] = 'Dental'
     #bupa_b_type = {'C': 'Clinic', 'H': 'Hospital', 'D': 'Dental', 'G': 'Optical', 'T': 'Hospital', 'W': 'Hospital', 'M': 'Maternity'}
     aia_dep_type = {'M': 'EE', 'S':'SP', 'C': 'CH', 'E': 'EE'}
     t_df['dep_type'].replace(aia_dep_type, inplace=True)
