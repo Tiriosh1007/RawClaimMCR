d101f31fa22f8a748d9138228f1a951975f9264e
Tiriosh1007 <49543074+Tiriosh1007@users.noreply.github.com>
2026-02-02 15:26:56 +0800

    upload

--- MemberCensus.py
+++ MemberCensus.py
@@ -529,7 +529,7 @@ class MemberCensus():
             temp_df['insurer'] = insurer
             # temp_df['policy_start_date'] = pd.to_datetime(temp_df['policy_start_date'])
             
-            if "policy_start_date" not in temp_df.columns:
+            """if "policy_start_date" not in temp_df.columns:
                 temp_df.rename(columns= {"Med. eff. date": 'policy_start_date'}, inplace=True)
                 _month = temp_df['policy_start_date'].min()[-4:]
                 print(_month)
@@ -540,7 +540,7 @@ class MemberCensus():
                 else:
                     temp_df['policy_start_date'] = pd.to_datetime("".join([_year, _month]), format="%Y%m%d")
             else:
-                temp_df['policy_start_date'] = pd.to_datetime(temp_df['policy_start_date'], format="%Y%m%d")
+                temp_df['policy_start_date'] = pd.to_datetime(temp_df['policy_start_date'], format="%Y%m%d")"""
 
             temp_df = _apply_policy_start(temp_df)
 
