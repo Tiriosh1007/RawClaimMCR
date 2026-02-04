c31f4984c88637216ba3a1e8aa2c86b57a1dab45
Tiriosh1007 <49543074+Tiriosh1007@users.noreply.github.com>
2026-02-04 17:23:24 +0800

    Fix date conversion for age calculation

--- MemberCensus.py
+++ MemberCensus.py
@@ -676,7 +676,7 @@ class MemberCensus():
             # temp_df['policy_start_date'] = pd.to_datetime(temp_df['policy_start_date']).dt.year.astype(int)
             temp_df['insurer'] = insurer
             temp_df = _apply_policy_start(temp_df, "%Y%m%d")
-            temp_df['age'] = self.date_conversion(temp_df['age'], format="%Y%m%d")
+            temp_df['age'] = self.date_conversion(temp_df['age'])
             temp_df['age'] = (temp_df["policy_start_date"] - temp_df['age']).dt.days / 365.25
             temp_df['age'] = temp_df['age'].astype('int')
             for col in self.cols:
